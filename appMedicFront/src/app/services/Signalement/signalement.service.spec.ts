913e8ebd92f3886d0593ee0c7725cfc94f94a88d
paulpp78 <perigault.paul@gmail.com>
2024-03-24 23:57:16 +0100

    Ajout du Workflow pour les tests Angular

--- appMedicFront/src/app/services/Signalement/signalement.service.spec.ts
+++ appMedicFront/src/app/services/Signalement/signalement.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from "@angular/core/testing";
 
 import { SignalementService } from "./signalement.service";
+import { HttpClientModule } from "@angular/common/http";
 
 describe("SignalementService", () => {
   let service: SignalementService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [SignalementService],
+    });
     service = TestBed.inject(SignalementService);
   });
 
