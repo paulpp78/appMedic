6fb5166eca307c91ef10457438bacd278ff076ff
paulpp78 <perigault.paul@gmail.com>
2024-06-04 04:20:56 +0200

    Modification du server Go et AJout d'une landing page

--- appMedicFront/src/app/app-routing.module.ts
+++ appMedicFront/src/app/app-routing.module.ts
@@ -1,24 +1,23 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {AnalyticsComponent} from './components/analytics/analytics.component';
-import {SignalementComponent} from './components/signalement/signalement.component';
-import {AuthGuardService} from './services/authGuard/auth-guard.service';
-import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AnalyticsComponent } from "./components/analytics/analytics.component";
+import { SignalementComponent } from "./components/signalement/signalement.component";
+import { AuthGuardService } from "./services/authGuard/auth-guard.service";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent},
+  { path: "", component: LandingPageComponent },
 
   {
-    path: 'signalement',
+    path: "signalement",
     component: SignalementComponent,
     canActivate: [AuthGuardService],
   },
-  {path: 'analytics', component: AnalyticsComponent},
+  { path: "analytics", component: AnalyticsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- appMedicFront/src/app/app.module.ts
+++ appMedicFront/src/app/app.module.ts
@@ -1,31 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavComponent } from './components/nav/nav.component';
-import { ButtonModule } from 'primeng/button';
-import { SidebarModule } from 'primeng/sidebar';
-import { AnalyticsComponent } from './components/analytics/analytics.component';
-import { ChartModule } from 'primeng/chart';
-import { AddSignalementComponent } from './components/signalement/add-signalement/add-signalement.component';
-import { GetListSignalementComponent } from './components/signalement/get-list-signalement/get-list-signalement.component';
-import { SignalementComponent } from './components/signalement/signalement.component';
-import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TableModule } from 'primeng/table';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AnalyticsByDayComponent } from './components/analytics/analytics-by-day/analytics-by-day.component';
-import { AnalyticsByWeekComponent } from './components/analytics/analytics-by-week/analytics-by-week.component';
-import { AnalyticsByMonthComponent } from './components/analytics/analytics-by-month/analytics-by-month.component';
-import { AnalyticsByYearComponent } from './components/analytics/analytics-by-year/analytics-by-year.component';
-import { AuthModule } from '@auth0/auth0-angular';
-import { environment } from '../environments/environment';
-import { AuthButtonComponent } from './components/auth/auth-button/auth-button.component';
-import { UserProfileComponent } from './components/auth/user-profile/user-profile.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { ButtonModule } from "primeng/button";
+import { SidebarModule } from "primeng/sidebar";
+import { AnalyticsComponent } from "./components/analytics/analytics.component";
+import { ChartModule } from "primeng/chart";
+import { AddSignalementComponent } from "./components/signalement/add-signalement/add-signalement.component";
+import { GetListSignalementComponent } from "./components/signalement/get-list-signalement/get-list-signalement.component";
+import { SignalementComponent } from "./components/signalement/signalement.component";
+import { InputTextModule } from "primeng/inputtext";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TableModule } from "primeng/table";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AnalyticsByDayComponent } from "./components/analytics/analytics-by-day/analytics-by-day.component";
+import { AnalyticsByWeekComponent } from "./components/analytics/analytics-by-week/analytics-by-week.component";
+import { AnalyticsByMonthComponent } from "./components/analytics/analytics-by-month/analytics-by-month.component";
+import { AnalyticsByYearComponent } from "./components/analytics/analytics-by-year/analytics-by-year.component";
+import { AuthModule } from "@auth0/auth0-angular";
+import { environment } from "../environments/environment";
+import { AuthButtonComponent } from "./components/auth/auth-button/auth-button.component";
+import { UserProfileComponent } from "./components/auth/user-profile/user-profile.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 
 @NgModule({
   declarations: [
